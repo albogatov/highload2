70d66d13f6f6003b55b5125b837055a4b8ce7bca
Sofya Inglikova <sofiainglikova@gmail.com>
2023-11-27 21:22:26 +0300

    fix: applicaton.yaml upd

---
+++ src/test/java/com/example/highload/AdminAPITests.java
@@ -0,0 +1,25 @@
+package com.example.highload;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AdminAPITests {
+
+    @Container
+    private static final PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:latest")
+            .withDatabaseName("highload")
+            .withUsername("high_user")
+            .withPassword("high_user");
+
+    @BeforeAll
+    public static void initUsers() {
+
+    }
+
+}
+
